d417db6ab614f72c6b3486dff2455315896563fe
Airencarme <Airencarme.Alanur@gmail.com>
2020-11-18 22:16:11 +0300

    23

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,6 +6,6 @@ def test_index(app, client):
     assert res.data == b"Base action"
 
 def test_v2(app, client):
-    res = client.get('/v2')
+    res = client.get('/oleynik')
     assert res.status_code == 200
     assert res.data == b"Hello from CI with GitHub Actions by Oleynik"
